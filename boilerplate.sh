eb1456f18f3b5a9d0c86cbd264af21618ab07fbe
rosindubh <p.welsby2@ntlworld.com>
2021-09-22 14:14:41 +0100

    create file structure

---
+++ boilerplate.sh
@@ -0,0 +1,15 @@
+
+>.env
+>.gitignore
+mkdir src
+mkdir src/controllers
+mkdir src/db
+mkdir src/middleware
+mkdir src/models
+mkdir src/routes
+>src/server.js
+>src/controllers/index.js
+>src/db/connection.js
+>src/middleware/index.js
+>src/models/index.js
+>src/routes/index.js
