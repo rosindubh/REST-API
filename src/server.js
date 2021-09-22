95a7cc0874b0283ca2aa34421be6b084921a68e9
rosindubh <p.welsby2@ntlworld.com>
2021-09-22 15:06:25 +0100

    install cors - edit server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 ///phil welsby - 22 sept 2021 - server.js file
 
+require("./db/connection");
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const helloRouter = require("./routes")
+
 app.use(express.json());
+app.use(cors());
 
 app.use(helloRouter)
 
