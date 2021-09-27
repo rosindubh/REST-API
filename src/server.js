f26a2efc55aa9f2d525fdf449a736e833105f61b
rosindubh <p.welsby2@ntlworld.com>
2021-09-27 23:25:38 +0100

    moved port const in server.js

--- src/server.js
+++ src/server.js
@@ -1,17 +1,17 @@
-///phil welsby - 22 sept 2021 - server.js file
+//phil welsby - 22 sept 2021 - server.js file
 
 require("./db/connection");
 const express = require("express");
 const cors = require("cors");
 const app = express();
 const helloRouter = require("./guitar/guitar.routes")
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
 
 app.use(helloRouter)
 
-const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server listening on port ${port}...`);
 });
